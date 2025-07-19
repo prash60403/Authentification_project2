444539231d59ec4f96ee73b8d5b790973cf124cb
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-19 08:38:53 +0530

    logout

--- AuthenticationProject/Core/urls.py
+++ AuthenticationProject/Core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('register/', views.RegisterView, name='register'),
     path('login/', views.LoginView, name='login'),
+    path('logout/', views.LogoutView, name='logout')
+
 ]
