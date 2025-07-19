4930d29644eef101910b359aac8cfcee3c916227
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-19 08:01:03 +0530

    loginview

--- AuthenticationProject/Core/views.py
+++ AuthenticationProject/Core/views.py
@@ -49,5 +49,22 @@ def RegisterView(request):
             return redirect('login')
 
     return render(request, 'register.html')
+
 def LoginView(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('home')
+        
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
+
     return render(request, 'login.html')
