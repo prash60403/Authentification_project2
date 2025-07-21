e13eb62b1c3b5504472a70d5f56704fd43f34403
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-21 13:10:28 +0530

    revert

--- AuthenticationProject/AuthenticationProject/settings.py
+++ AuthenticationProject/AuthenticationProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o(43%rt7sfd4n08apj10&ow439=6%j(z889^fn2!xl)z5r4o+l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://authentification-project2.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
